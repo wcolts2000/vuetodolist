60741e6d8860872230106799604c4444d8ea46eb
Sean Pheneger <41925787+wcolts2000@users.noreply.github.com>
2019-01-27 15:21:19 -0800

    Add files via upload

---
+++ src/views/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>This is a sample vue and vue router app: TodoList app v1.0.0. It is a part of the Vue Crash Course from Brad Traversy on YouTube.</p>
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,93 @@
+<template>
+  <div id="app">
+    <AddTodo v-on:add-todo="addTodo"/>
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
+  </div>
+</template>
+
+<script>
+import AddTodo from "../components/AddTodo";
+import Todos from "../components/Todos";
+import axios from "axios";
+
+export default {
+  name: "home",
+  components: {
+    Todos,
+    AddTodo
+  },
+  data() {
+    return {
+      // todos: [
+      //   {
+      //     id: 1,
+      //     title: "Todo One",
+      //     completed: false
+      //   },
+      //   {
+      //     id: 2,
+      //     title: "Todo Two",
+      //     completed: false
+      //   },
+      //   {
+      //     id: 3,
+      //     title: "Todo Three",
+      //     completed: true
+      //   }
+      // ]
+      todos: []
+    };
+  },
+  methods: {
+    deleteTodo(id) {
+      axios
+        .delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+        .then(res => (this.todos = this.todos.filter(todo => todo.id !== id)))
+        .catch(err => console.log(err));
+    },
+    addTodo(newTodo) {
+      const { title, completed } = newTodo;
+
+      axios
+        .post("https://jsonplaceholder.typicode.com/todos", {
+          title,
+          completed
+        })
+        .then(res => (this.todos = [...this.todos, res.data]))
+        .catch(err => console.log(err));
+    }
+  },
+  created() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos?_limit=15")
+      .then(res => (this.todos = res.data))
+      .catch(err => console.log(err));
+  }
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
+  line-height: 1.4;
+}
+
+.btn {
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+
+.btn:hover {
+  background: #666;
+}
+</style>
